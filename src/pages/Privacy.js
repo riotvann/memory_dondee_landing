83ac8e43e45b0b697214c0e8eb0d33c3fe524197
Jorge Gámez <161256943+riotvann@users.noreply.github.com>
2025-02-13 14:09:22 -0700

    first commit

---
+++ src/pages/Privacy.js
@@ -0,0 +1,46 @@
+
+
+
+
+function Privacy () {
+
+    return <>
+        <h1>Privacy Policy</h1>
+        
+        <p>
+        1. Introduction
+        Welcome to Memory Dondee. Your privacy is important to us. This Privacy Policy explains how we collect, use, disclose, and protect your information when you use our mobile application.
+
+        </p>
+        <p>
+
+        2. Information We Collect
+        Our app does not collect, store, or share any personally identifiable information.
+
+        </p>
+     
+        <p>
+        3. Children's Privacy
+        If your target audience includes children under 13, ensure compliance with the Children's Online Privacy Protection Act (COPPA). Our app does not knowingly collect personal data from children. If we learn that we have collected such data, we will delete it immediately.
+
+        </p>
+
+        <p>
+        4. Changes to This Privacy Policy
+        We may update this policy from time to time. Any changes will be posted within the app and on our official website. We encourage users to review this policy periodically.
+
+        </p>
+      
+        <p>
+        5. Contact Us
+        If you have any questions about this Privacy Policy, please contact us at:
+        nuageproducciones@gmail.com
+        </p>
+
+        <p>
+        By using Memory Dondee, you agree to this Privacy Policy.
+        </p>
+    </>
+}
+
+export default Privacy;
